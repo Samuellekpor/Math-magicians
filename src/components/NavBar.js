3fb875baafbdd8f7330a57fe1e91e5434e0778e3
Samuellekpor <samuellekpor@gmail.com>
2022-11-16 11:38:57 +0000

    Add styling to the pages

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
+  const activeStyle = {
+    color: '#da6900',
+    textDecoration: 'underline',
+  };
+
   return (
     <div>
       <nav>
         <h1 className="logo">Math Magicians</h1>
         <ul className="navLinks">
-          <li className="link"><Link to="/">Home</Link></li>
-          <li className="link"><Link to="/calculator">Calculator</Link></li>
-          <li className="link"><Link to="/quotes">Quotes</Link></li>
+          <li className="link">
+            <NavLink
+              to="/"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="link">
+            <NavLink
+              to="/calculator"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Calculator
+            </NavLink>
+          </li>
+          <li className="link">
+            <NavLink
+              to="/quotes"
+              style={({ isActive }) => (isActive ? activeStyle : undefined)}
+            >
+              Quotes
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </div>
--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -3,9 +3,9 @@ import React from 'react';
 function Quotes() {
   return (
     <div>
-      <h3>
+      <h3 className="quote">
         Mathematics is not about numbers, equations, computations or algorithms:
-        it's about understanding.-William Paul Thurston
+        it&apos;s about understanding.-William Paul Thurston
       </h3>
     </div>
   );
