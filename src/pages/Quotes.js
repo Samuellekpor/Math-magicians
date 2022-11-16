ca9acadbd25a2195330ca1111237013719ea80a3
Samuellekpor <samuellekpor@gmail.com>
2022-11-16 08:53:46 +0000

    Add quotes pages

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Quotes() {
   return (
-    <div>Quotes</div>
+    <div>
+      <h3>
+        Mathematics is not about numbers, equations, computations or algorithms:
+        it's about understanding.-William Paul Thurston
+      </h3>
+    </div>
   );
 }
 
