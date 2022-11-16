1962771325bf993cef2d00f66f2729c33cd665c8
Samuellekpor <samuellekpor@gmail.com>
2022-11-16 08:42:16 +0000

    Add home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,37 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h3 className="title">Welcome to our page</h3>
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        magni impedit odit laborum voluptas labore, suscipit ut recusandae?
+        Blanditiis maxime unde earum, laudantium atque sed sapiente iste dignissimos
+        perferendis vero id, inventore magni molestiae eum accusamus
+        culpa ipsum delectus ab, fugit animi tenetur. Veritatis iusto aspernatur
+        repellendus consectetur voluptatibus aliquam voluptas perspiciatis
+        perferendis explicabo laborum ut, iure magnam voluptate id molestias quas?
+        Deserunt nihil esse molestiae dignissimos! Nihil cupiditate, tempora,
+        iste ipsum, optio facilis iusto quidem officiis ipsa veritatis eius eveniet!
+        Laborum libero necessitatibus consectetur quas possimus,
+        nam laboriosam quidem, ipsa atque facilis vel nulla ducimus dolorem
+        numquam veniam voluptatum.
+      </p>
+      <p className="text">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium
+        magni impedit odit laborum voluptas labore, suscipit ut recusandae?
+        Blanditiis maxime unde earum, laudantium atque sed sapiente iste dignissimos
+        perferendis vero id, inventore magni molestiae eum accusamus
+        culpa ipsum delectus ab, fugit animi tenetur. Veritatis iusto aspernatur
+        repellendus consectetur voluptatibus aliquam voluptas perspiciatis
+        perferendis explicabo laborum ut, iure magnam voluptate id molestias quas?
+        Deserunt nihil esse molestiae dignissimos! Nihil cupiditate, tempora,
+        iste ipsum, optio facilis iusto quidem officiis ipsa veritatis eius eveniet!
+        Laborum libero necessitatibus consectetur quas possimus,
+        nam laboriosam quidem, ipsa atque facilis vel nulla ducimus dolorem
+        numquam veniam voluptatum.
+      </p>
+    </div>
   );
 }
 
